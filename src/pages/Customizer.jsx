dc4bfb05577e8dfd7234be92d2915abcdc60d810
RaitisVilums <rvilums80@gmail.com>
2023-07-18 08:57:26 +0300

    pizza

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -20,9 +20,29 @@ const Customizer = () => {
   const snap = useSnapshot(state);
   const [file, setfile] = useState("");
 
+  const [prompt, setPrompt] = useState("");
+  const [generatingImg, setGeneratingImg] = useState(false);
+
+  const [activeEditorTab, setActiveEditorTab] = useState("");
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  });
+
   // show tab content = depending on the activeTab
 
-  const generateTabContent = () => {};
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker />;
+      case "filepicker":
+        return <FilePicker />;
+      case "aipicker":
+        return <AIPicker />;
+      default:
+        break;
+    }
+  };
 
   return (
     <AnimatePresence>
@@ -36,8 +56,14 @@ const Customizer = () => {
             <div className="flex items-center min-h-screen">
               <div className="editortabs-container tabs">
                 {EditorTabs.map((tab) => (
-                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                  <Tab
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => setActiveEditorTab(tab.name)}
+                  />
                 ))}
+
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
